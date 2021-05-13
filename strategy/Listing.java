d728df39054de7d2faf02ebddc8e5fa570258817
Andriani <theblue_squirrel@yahoo.com>
2021-05-13 11:09:14 -0400

    create SortBehavior.java and Listing.java

---
+++ strategy/Listing.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Listing {
+    
+    private String title;
+    private ArrayList<String> items;
+
+}
---
+++ strategy/SortBehavior.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public interface SortBehavior {
+
+    public default void sort(ArrayList<String> data) {
+        //TODO create sorting algorithm 
+    }
+}
