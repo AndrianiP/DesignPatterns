d51eef16d981df00d78cff29fe181c7851f0179d
Andriani <theblue_squirrel@yahoo.com>
2021-05-15 20:37:01 -0400

    pretty sure this is correct

--- observer/Subject.java
+++ observer/Subject.java
@@ -1,8 +1,9 @@
 package observer;
 
-
 public interface Subject {
     public void registerObserver(Observer observer);
+
     public void removeObserver(Observer observer);
+
     public void notifyObservers();
 }
