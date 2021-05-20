cbe6df1d0ae6d4c177014471f5a525f4e985c783
Andriani <theblue_squirrel@yahoo.com>
2021-05-20 00:27:21 -0400

    commit finished Symbols

--- decorator/Symbols.java
+++ decorator/Symbols.java
@@ -4,42 +4,52 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.*;
 
-
 public class Symbols extends PasswordDecorator {
-
 
     public Symbols(Password passwordBeginning) {
         super(passwordBeginning);
-        int counter = 0;
         this.password = passwordBeginning.getPassword();
         HashMap<String, String> conversion = new HashMap<String, String>();
-        ArrayList<CharSequence> tempArrayList = new ArrayList<CharSequence>();
-
+        ArrayList<String> tempArrayList = new ArrayList<String>();
         for (int i = 0; i < this.password.length(); i++) {
             char temp = this.password.charAt(i);
-            conversion.put(String.valueOf(temp), String.valueOf(temp));
+
+            switch (String.valueOf(temp)) {
+                case "a":
+
+                    tempArrayList.add("@");
+                    break;
+                case "b":
+                    tempArrayList.add("8");
+                    break;
+                case "e":
+                    tempArrayList.add("3");
+                    break;
+                case "g":
+                    tempArrayList.add("9");
+                    break;
+                case "i":
+                    tempArrayList.add("!");
+                    break;
+                case "o":
+                    tempArrayList.add("0");
+                    break;
+                case "s":
+                    tempArrayList.add("$");
+                    break;
+                case "t":
+                    tempArrayList.add("7");
+                    break;
+                default:
+                    tempArrayList.add(String.valueOf(temp));
+                    break;
+            }
         }
-        //conversion.put("a", "@");
-        //conversion.put("b", "8");
-        //conversion.put("e", "3");
-        //conversion.put("g", "9");
-        //conversion.replace("i", "!");
-        //conversion.put("o", "o");
-        //conversion.put("s", "0");
-      //  conversion.put("t", "7");
-        /*
-        for(conversion.entrySet().forEach( entry -> {
-            
-            char tempChar = entry.getValue().charAt(0);
-            tempArrayList.add(tempChar);
-            this.password = String.join("", tempArrayList);
-            i++;
-        });
-        */
-        //Some stackoverflow trying to currently debug and see how it works from what i read
-        String reduced = conversion.values().stream().map(String::toString).reduce("", String::concat);
-        this.password = reduced;
-    }  
+
+       
+        this.password = String.join("", tempArrayList);
+    }
+
     public String getPassword() {
 
         return this.password;
