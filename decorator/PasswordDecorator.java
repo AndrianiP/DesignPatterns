e55464ecece15462fb6ead84f0fcdbbd5def2e74
Andriani <theblue_squirrel@yahoo.com>
2021-05-19 23:52:04 -0400

    Symbols doesnt correctly replace the values
    I understand the concept quite well i just have failed to change the text in the right still debugging. If i had an hour or two i could probably do it!

--- decorator/PasswordDecorator.java
+++ decorator/PasswordDecorator.java
@@ -4,7 +4,7 @@ public abstract class PasswordDecorator extends Password {
     protected Password passwordBeginning;
 
     public PasswordDecorator(Password passwordBeginning) {
-        
+        this.password = super.password;
 
     }
 
--- decorator/Symbols.java
+++ decorator/Symbols.java
@@ -1,13 +1,46 @@
 package decorator;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.*;
+
+
 public class Symbols extends PasswordDecorator {
-    public Symbols(Password passwordBeginning){
+
+
+    public Symbols(Password passwordBeginning) {
         super(passwordBeginning);
-        
+        int counter = 0;
+        this.password = passwordBeginning.getPassword();
+        HashMap<String, String> conversion = new HashMap<String, String>();
+        ArrayList<CharSequence> tempArrayList = new ArrayList<CharSequence>();
 
-    }
-    public String getPassword(){
+        for (int i = 0; i < this.password.length(); i++) {
+            char temp = this.password.charAt(i);
+            conversion.put(String.valueOf(temp), String.valueOf(temp));
+        }
+        conversion.put("a", "@");
+        conversion.put("b", "8");
+        conversion.put("e", "3");
+        conversion.put("g", "9");
+        conversion.put("i", "!");
+        conversion.put("o", "o");
+        conversion.put("s", "0");
+        conversion.put("t", "7");
+        /*
+        for(conversion.entrySet().forEach( entry -> {
+            
+            char tempChar = entry.getValue().charAt(0);
+            tempArrayList.add(tempChar);
+            this.password = String.join("", tempArrayList);
+            i++;
+        });
+        */
+        String reduced = conversion.values().stream().map(Object::toString).reduce("", String::concat);
+        this.password = reduced;
+    }  
+    public String getPassword() {
 
-        return password;
+        return this.password;
     }
 }
