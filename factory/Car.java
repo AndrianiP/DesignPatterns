b8946145fb40798d5107e13da3cfb19d616234ed
Andriani <theblue_squirrel@yahoo.com>
2021-05-22 16:00:05 -0400

    wrote Car props

--- factory/Car.java
+++ factory/Car.java
@@ -1,8 +1,38 @@
 package factory;
+
 import java.util.*;
 
 public abstract class Car {
     private String make;
     private String model;
     protected ArrayList<Accessories> Accessories = new ArrayList<Accessories>();
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    public void assemble() {
+
+    }
+
+    protected void addFrame() {
+
+    }
+
+    protected void addWheels() {
+
+    }
+    protected void addEngine() {
+
+    }
+    protected void addWindows() {
+
+    }
+    protected void addAccessories() {
+
+    }
+    protected void displayAccessories() {
+        
+    }
 }
--- factory/CarFactory.java
+++ factory/CarFactory.java
@@ -1,5 +1,7 @@
 package factory;
 
 public class CarFactory {
-    
+    public Car createCar(String type, String make, String model) {
+        
+    }
 }
